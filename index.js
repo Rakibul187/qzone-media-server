190ff2ffa8f4ebe5fcc8c9deb390caa33f3ff37b
rakib <rakibul241221@gmail.com>
2022-12-28 21:04:16 +0600

    connect with mongodb

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const cors = require('cors');
 const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.farjvzi.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+console.log(uri)
+async function run() {
+    try {
+
+    }
+    finally {
+
+    }
+}
+run().catch(e => console.error(e))
+
+
 
 app.get('/', (req, res) => {
     res.send('Qzone media server is running')
