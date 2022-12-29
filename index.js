c134187b1c3a06c59525dc3bcb9f79a5d06ff111
rakib <rakibul241221@gmail.com>
2022-12-29 18:37:31 +0600

    single post by id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -11,7 +11,6 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.farjvzi.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-console.log(uri)
 async function run() {
     try {
         const postCollection = client.db('qzonemdeia').collection('postData')
@@ -21,6 +20,25 @@ async function run() {
             const result = await postCollection.insertOne(data)
             res.send(result)
         })
+
+        app.get('/posts', async (req, res) => {
+            const query = {}
+            const posts = await postCollection.find(query).toArray()
+            res.send(posts)
+        })
+
+        app.get('/postdetails/:id', async (req, res) => {
+            const id = req.params.id
+            console.log(id)
+            const query = {
+                _id: ObjectId(id)
+            }
+            console.log(query)
+            const post = await postCollection.findOne(query)
+            res.send(post)
+        })
+
+
     }
     finally {
 
